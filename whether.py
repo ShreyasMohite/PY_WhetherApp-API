53ead07a06c70375b1e9a970c9ebf26c1d8a4928
shreyashack <shreyasmohite11123@gmail.com>
2020-09-08 19:07:04 +0530

    First commit

---
+++ whether.py
@@ -0,0 +1,108 @@
+from tkinter import *
+import tkinter.messagebox
+import requests
+
+class whether_app:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Whether App")
+        self.root.geometry("500x300")
+        self.root.resizable(0,0)
+        self.root.iconbitmap("whether.ico")
+
+
+        search_what=StringVar()
+
+        
+
+
+
+    #====================================================
+        def on_enter1(e):
+            But_search['background']="black"
+            But_search['foreground']="cyan"
+  
+        def on_leave1(e):
+            But_search['background']="SystemButtonFace"
+            But_search['foreground']="SystemButtonText"    
+
+        def clear():
+            TXT.delete('1.0',END)
+
+        def search():
+            try:
+                
+                if search_what.get()=="":
+                    tkinter.messagebox.askretrycancel("Information","Your internet is not working please try agin",icon="info")
+
+                else:
+                    whether_key='43ee602cd3f013c1a0c2fa793155236e'
+                    url='https://api.openweathermap.org/data/2.5/weather'
+                    params={'APPID':whether_key,"q":search_what.get()}
+                    response=requests.get(url,params=params)
+                    whether=response.json()
+                    #print("Name",whether['name'])
+                    a=whether['weather'][0]['description']
+                    #print(whether['main']['temp'])
+                    #print(whether['main']['humidity'])
+                    #print(whether['wind']['speed'])
+                    #print(whether['sys']['country'])
+                    #print(whether['timezone'])
+
+                    clear()
+                    TXT.insert(END,"Name : ")
+                    TXT.insert(END,whether['name'])
+                    TXT.insert(END,"\n")
+                    TXT.insert(END,"Whether description : ")
+                    TXT.insert(END,a)
+                    TXT.insert(END,"\n")
+                    TXT.insert(END,"Temperature : ")
+                    TXT.insert(END,whether['main']['temp'])
+                    TXT.insert(END,"\n")
+                    TXT.insert(END,"humidity : ")
+                    TXT.insert(END,whether['main']['humidity'])
+                    TXT.insert(END,"\n")
+                    TXT.insert(END,"Wind Speed : ")
+                    TXT.insert(END,whether['wind']['speed'])
+                    TXT.insert(END,"\n")
+                    TXT.insert(END,"Country : ")
+                    TXT.insert(END,whether['sys']['country'])
+                    TXT.insert(END,"\n")
+                    TXT.insert(END,"Timezone : ")
+                    TXT.insert(END,whether['timezone'])
+            except:
+                tkinter.messagebox.askretrycancel("Info","Somthing went wrong or network error")
+
+
+                
+    #=====================Frame=======================
+        Main_Frame=Frame(self.root,width=500,height=300,relief=RIDGE,bd=3)
+        Main_Frame.place(x=0,y=0)
+
+        top_frame=Frame(Main_Frame,width=495,height=90,relief=RIDGE,bd=3)
+        top_frame.place(x=0,y=0)
+
+        Bottom_Frame=Frame(Main_Frame,width=495,height=205,relief=RIDGE,bd=3)
+        Bottom_Frame.place(x=0,y=90)
+
+        #============================================================
+        lab_top=LabelFrame(top_frame,text="Search Whether",font=('times new roman',12,'bold'),width=490,height=85,bg="#db025f")
+        lab_top.place(x=0,y=0)
+
+        Ent_search=Entry(lab_top,font=('times new roman',14,'bold'),width=30,bd=2,textvariable=search_what)
+        Ent_search.place(x=20,y=10)
+
+        But_search=Button(lab_top,text="Search",font=('times new roman',11,'bold'),relief=RIDGE,bd=3,width=14,cursor="hand2",command=search)
+        But_search.place(x=330,y=8)
+        But_search.bind("<Enter>",on_enter1)
+        But_search.bind("<Leave>",on_leave1)
+
+        #===============================
+        TXT=Text(Bottom_Frame,width=53,height=10,font=('times new roman',13,'bold'),bg="#afc5f8",bd=5)
+        TXT.place(x=0,y=0)
+
+
+if __name__ == "__main__":
+    root=Tk()
+    app=whether_app(root)
+    root.mainloop()
